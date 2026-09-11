c01003db215c56e8b9984d5c36e9620097bba4fe
maarcus_andersson <marcus.andersson@stud.sti.se>
2026-09-11 13:26:00 +0200

    Add setup_roles.sql

---
+++ roles_management/setup_roles.sql
@@ -0,0 +1,52 @@
+USE ROLE USERADMIN;
+
+CREATE ROLE ice_cream_reader COMMENT = "Able to read ice_cream_db";
+CREATE ROLE ice_cream_writer COMMENT = "Able to do CRUD operations on ice_cream_db";
+
+CREATE ROLE ice_cream_analyst COMMENT = "Able to create views on ice_cream_db";
+
+SHOW ROLES;
+
+USE ROLE SECURITYADMIN;
+
+GRANT USAGE ON WAREHOUSE dev_wh TO ROLE ice_cream_reader;
+
+GRANT USAGE ON DATABASE ice_cream_db TO ROLE ice_cream_reader;
+
+GRANT USAGE ON ALL SCHEMAS IN DATABASE ice_cream_db TO ROLE ice_cream_reader;
+
+-- READ -- SELECT
+
+GRANT SELECT ON ALL TABLES IN SCHEMA ice_cream_db.public TO ice_cream_reader;
+
+GRANT SELECT ON FUTURE TABLES IN SCHEMA ice_cream_db.public TO ice_cream_reader;
+
+SHOW GRANTS TO ROLE ice_cream_reader;
+
+SHOW FUTURE GRANTS IN SCHEMA ice_cream_db.public;
+
+GRANT ROLE ice_cream_reader TO ROLE ice_cream_writer;
+
+SHOW GRANTS TO ROLE ice_cream_writer;
+
+-- grant CRUD operations to ice_cream_writer
+
+GRANT
+    INSERT, UPDATE, DELETE
+    ON ALL TABLES
+    IN SCHEMA ice_cream_db.public TO ROLE ice_cream_writer;
+
+GRANT
+    INSERT, UPDATE, DELETE
+    ON FUTURE TABLES
+    IN SCHEMA ice_cream_db.public TO ROLE ice_cream_writer;
+
+GRANT CREATE TABLE ON SCHEMA ice_cream_db.public TO ROLE ice_cream_writer;
+
+SHOW GRANTS TO ROLE ice_cream_writer;
+
+SHOW FUTURE GRANTS IN SCHEMA ice_cream_db.public;
+
+USE ROLE USERADMIN;
+
+GRANT ROLE ice_cream_writer to USER mander;
